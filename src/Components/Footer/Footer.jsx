c25db887cc852afaf79569253dda8a57341e2bc8
Aleesha Nishan <Aleesha@Aleeshas-MacBook-Pro.local>
2024-09-22 22:13:00 +0530

    4th Commit: Completed all features of footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,6 +4,24 @@ import './Footer.css'
 function Footer() {
   return (
     <>
+    <footer className="bg-cover">
+        <hr className="footer-divider" />
+        <div className="footer-bottom">
+            <div className="container">
+                <div className="row gy-3 justify-content-between">
+                    <div className="col-auto footer-left">
+                        <h1 className="mb-0">Wefo</h1>
+                    </div>
+
+                    <div className="col footer-right">
+                        <h4 className="mb-0">Privacy</h4>
+                        <h4 className="mb-0">Term</h4>
+                        <h4 className="mb-0">Security</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     </>
   )
 }
