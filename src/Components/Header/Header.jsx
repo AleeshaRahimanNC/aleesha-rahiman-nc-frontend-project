cb3e7bdf85dcf9709f9e04a62818f194b984c520
Aleesha Nishan <Aleesha@Aleeshas-MacBook-Pro.local>
2024-09-22 19:26:14 +0530

    3rd Commit: Completed all features for header.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightDots } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <>
-    <h1>We create award winning website</h1>
+      <div className="header-wrapper">
+        <div className="content">
+          
+            <div className="title">
+              <h1>We create award winning website</h1>
+            </div>
+          
+
+            <div className="para">
+              <h6>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                aspernatur possimus suscipit iusto repudiandae nihil quibusdam
+                itaque? Expedita, fugiat voluptates!
+              </h6>
+            </div>
+            
+
+            <form className="d-flex" role="search">
+              <button className="btn box-out" type="submit">
+                Explore
+                <FontAwesomeIcon icon={faArrowUpRightDots} />
+              </button>
+              </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
